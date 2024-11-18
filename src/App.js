6c149772c7818fc34add72f5a8437d3928177cfe
Humberto Islas <islas333@gmail.com>
2024-11-18 14:25:45 -0600

    Ajuste imagen

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div className="container mx-auto px-4 py-4">
         <div className="grid grid-cols-1">
           <div className='items-center justify-center'>
-            <img src="./logo.png" alt="Logo" className="mx-auto" />
+            <img src="/Logo.png" alt="Logo" className="mx-auto" />
           </div>
           <div>
             Bienvenido a Mi Consultorio V1.0, esta es una aplicación de escritorio para la gestión de consultorios médicos.
