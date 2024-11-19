3646fe8f611843b2f8cc8db3bd0c88cdc1b7d715
Humberto Islas <islas333@gmail.com>
2024-11-18 18:54:16 -0600

    Actualizacion

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import HeaderComponent from './Components/Header/Header.component';
+import { FaBusinessTime, FaCalculator, FaCalendarAlt, FaCashRegister, FaDonate } from "react-icons/fa";
 
 function App() {
   return (
     <>
       <HeaderComponent />
       <div className="container mx-auto px-4 py-4">
-        <div className="grid grid-cols-1">
-          <div className='items-center justify-center'>
-            <img src="/Logo.png" alt="Logo" className="mx-auto" />
-          </div>
-          <div>
-            Bienvenido a Mi Consultorio V1.0, esta es una aplicación de escritorio para la gestión de consultorios médicos.
+
+        <div className="grid grid-cols-1 text-center">
+          <h1 className="text-2xl font-bold">PROXIMAMENTE</h1>
+        </div>
+
+        <div className="grid grid-cols-3">
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2 text-center">
+                <FaDonate />
+              </div>
+              <div className="font-bold text-xl mb-2 text-center">MIS FINANZAS</div>
+              <p className="text-gray-700 text-base">
+                Modulo de finanzas para llevar un control de los ingresos y egresos del consultorio.
+              </p>
+            </div>
           </div>
         </div>
+
       </div>
     </>
   );
--- src/Components/Header/Header.component.js
+++ src/Components/Header/Header.component.js
@@ -1,15 +1,27 @@
 
 const HeaderComponent = () => {
   return (
-    <header className="bg-gray-200 text-gray-600 py-4">
+    <header className="bg-teal-800 text-gray-600 py-4">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-3 gap-4 items-center">
           <div>
-            <h1 className="text-2xl font-bold">Mi consultorio</h1>
+            {/* <h1 className="text-2xl font-bold">Mi consultorio</h1> */}
           </div>
           <div></div>
-          <div className="text-right text-sm">Contacto:<br />islas333@gmail.com</div>
+          <div className="text-right text-sm text-white">Contacto:<br />islas333@gmail.com</div>
         </div>
+      </div>
+      <div className="container mx-auto px-4 py-4">
+                
+        <div className="grid grid-cols-6">
+          <div className='col-start-2 col-span-4 flex justify-center'>
+            <img src="/Logo_white.png" alt="Logo" width="350" />
+          </div>
+          <div className='col-start-2 col-span-4 text-white font-bold text-center pt-10'>
+            Bienvenido a Mi Consultorio V1.0, esta es una aplicación de escritorio para la gestión de consultorios médicos. En este portal encontraras las actualizaciones y mejoras de la aplicación.
+          </div>
+        </div>
+
       </div>
     </header>
   )
