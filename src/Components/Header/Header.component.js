c3c6cbf78fd31aa34be07bf7148554989ff2a6e8
Humberto Islas <islas333@gmail.com>
2024-11-19 22:07:55 -0600

    Ajuste imagenes

--- src/Components/Header/Header.component.js
+++ src/Components/Header/Header.component.js
@@ -15,7 +15,7 @@ const HeaderComponent = () => {
                 
         <div className="grid grid-cols-6">
           <div className='col-start-2 col-span-4 flex justify-center'>
-            <img src="/Logo_white.png" alt="Logo" width="350" />
+            <img src="/Logo_white_2.png" alt="Logo" width="350" />
           </div>
           <div className='col-start-2 col-span-4 text-white font-bold text-center pt-10'>
             Bienvenido a Mi Consultorio V1.0, esta es una aplicación de escritorio para la gestión de consultorios médicos. En este portal encontraras las actualizaciones y mejoras de la aplicación.
